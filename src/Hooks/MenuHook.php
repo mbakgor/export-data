5e73d3af5af350416ae1ae48df43016ac98c92dd
mbakgor <bakgormuhammed@gmail.com>
2024-02-27 16:34:56 +0300

    0.1.6

--- src/Hooks/MenuHook.php
+++ src/Hooks/MenuHook.php
@@ -6,41 +6,5 @@ use App\Plugins\Hooks\MenuEntryHook;
 
 class MenuHook extends MenuEntryHook
 {
-    protected $attributes = [];
-
-    public function __get($name)
-    {
-        if ($name == 'view') {
-            return $this->attributes[$name] ?? null;
-        }
-
-        return parent::__get($name);
-    }
-
-    public function __set($name, $value)
-    {
-        if ($name == 'view') {
-            $this->attributes[$name] = $value;
-        } else {
-            parent::__set($name, $value);
-        }
-    }
-
-    public function __isset($name)
-    {
-        if ($name == 'view') {
-            return isset($this->attributes[$name]);
-        }
-
-        return parent::__isset($name);
-    }
-
-    public function __unset($name)
-    {
-        if ($name == 'view') {
-            unset($this->attributes[$name]);
-        } else {
-            parent::__unset($name);
-        }
-    }
+    public $view = 'export-data::menu.main';
 }
